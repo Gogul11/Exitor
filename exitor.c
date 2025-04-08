74b0e3098dadef04d236420d4d713a58971ba2b2
Gogul11 <gogu0811@gmail.com>
2025-04-08 22:14:40 +0530

    added color

--- exitor.c
+++ exitor.c
@@ -13,11 +13,11 @@ void exitorPrint()
 {
     printf(1, "\n");
 
-    printf(1, " EEEEE  X   X  III  TTTTT  OOOO  RRRR \n");
-    printf(1, " EE      X X    I     T    O  O  R  R \n");
-    printf(1, " EEEE     X     I     T    O  O  RRRR \n");
-    printf(1, " EE      X X    I     T    O  O  R R  \n");
-    printf(1, " EEEEE  X   X  III    T    OOOO  R  R \n");
+    printf(1, " \033[0;31mEEEEE\033[0;m  \033[0;34mX   X\033[0;m  \033[0;31mIII\033[0;m  \033[0;34mTTTTT\033[0;m  \033[0;31mOOOO\033[0;m  \033[0;34mRRRR\033[0;m \n");
+    printf(1, " \033[0;31mEE   \033[0;m  \033[0;34m X X \033[0;m  \033[0;31m I \033[0;m  \033[0;34m  T  \033[0;m  \033[0;31mO  O\033[0;m  \033[0;34mR  R\033[0;m \n");
+    printf(1, " \033[0;31mEEEE \033[0;m  \033[0;34m  X  \033[0;m  \033[0;31m I \033[0;m  \033[0;34m  T  \033[0;m  \033[0;31mO  O\033[0;m  \033[0;34mRRRR\033[0;m \n");
+    printf(1, " \033[0;31mEE   \033[0;m  \033[0;34m X X \033[0;m  \033[0;31m I \033[0;m  \033[0;34m  T  \033[0;m  \033[0;31mO  O\033[0;m  \033[0;34mR R \033[0;m \n");
+    printf(1, " \033[0;31mEEEEE\033[0;m  \033[0;34mX   X\033[0;m  \033[0;31mIII\033[0;m  \033[0;34m  T  \033[0;m  \033[0;31mOOOO\033[0;m  \033[0;34mR  R\033[0;m \n");
 }
 
 int main()
--- exitorBase/hi.c
+++ exitorBase/hi.c
@@ -2,6 +2,6 @@
 // #include "/printf.h"
 
 void sayHI(){
-    printf(1, "hi da");
+    printf(1, "\033[0;31m hi da\033[0;m\n");
     exit();
 }
