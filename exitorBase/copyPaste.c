00e1a4a3aef6ba61cdb508c0c3e755eb1cae1b56
GUNASEKHAR5 <kudimigunasekhar@gmail.com>
2025-04-18 16:16:02 +0000

    copypaste

---
+++ exitorBase/copyPaste.c
@@ -0,0 +1,34 @@
+#include "../types.h"
+#include "../user.h"
+
+#define CLIPBOARD_SIZE 1024
+
+static char clipboard[CLIPBOARD_SIZE];
+
+void copyText(char* src,int start,int end) {
+    if (!src) return;
+    if(end<start){
+        printf(1,"The start line is greater than end line!");
+        return;
+    }
+    int i=0;
+    int cnt=0;
+    while(src[i]!='\0' && cnt<(start-1)){
+        if(src[i++]=='\n')cnt++;
+    }
+    if(cnt!=start-1){
+        printf(1,"The buffer does not contains such lines!!");
+        return;
+    }
+    int j=0;
+    while(src[i]!='\0' && cnt<end){
+       clipboard[j++]=src[i];
+       if(src[i]=='\n')cnt++;
+       i++;
+    }
+    return;
+}
+
+char* pasteText() {
+    return clipboard;
+}
