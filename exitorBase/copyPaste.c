64d81332e75abe4cebceaf2433202528614557fe
Gogul11 <gogu0811@gmail.com>
2025-04-20 10:09:29 +0530

    error2

--- exitorBase/copyPaste.c
+++ exitorBase/copyPaste.c
@@ -1,38 +1,36 @@
-#include "../types.h"
-#include "../user.h"
+// #include "../types.h"
+// #include "../user.h"
 
-#define CLIPBOARD_SIZE 1024
+// #define CLIPBOARD_SIZE 1024
 
-static char clipboard[CLIPBOARD_SIZE];
+// static char clipboard[CLIPBOARD_SIZE];
 
-void copy(char* src,int start,int end) {
-    if (!src) return;
 
-  
+// void copy(char* src, int start, int end) {
+//     if (!src || start < 1 || end < start) return;
+    
+//     // Reset clipboard before copying
+//     memset(clipboard, 0, CLIPBOARD_SIZE);
+    
+//     int i = 0;
+//     int cnt = 1; // Line numbers start at 1
+//     int j = 0;
+    
+//     // Find start line
+//     while (src[i] && cnt < start) {
+//         if (src[i] == '\n') cnt++;
+//         i++;
+//     }
+    
+//     // Copy until end line or buffer full
+//     while (src[i] && cnt <= end && j < CLIPBOARD_SIZE - 1) {
+//         clipboard[j++] = src[i++];
+//         if (src[i-1] == '\n') cnt++;
+//     }
+    
+//     clipboard[j] = '\0'; // Ensure null termination
+// }
 
-    if(end<start){
-        printf(1,"The start line is greater than end line!");
-        return;
-    }
-    int i=0;
-    int cnt=0;
-    while(src[i]!='\0' && cnt<(start-1)){
-        if(src[i++]=='\n')cnt++;
-    }
-    if(cnt!=start-1){
-        printf(1,"The buffer does not contains such lines!!");
-        return;
-    }
-    int j=0;
-    while(src[i]!='\0' && cnt<end){
-       clipboard[j++]=src[i];
-       if(src[i]=='\n')cnt++;
-       i++;
-    }
-    clipboard[j] = '\0';
-    return;
-}
-
-char* paste() {
-    return clipboard;
-}
+// char* paste() {
+//     return clipboard;
+// }
